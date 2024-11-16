a342f89ccaeb854994f29a8142ab12b5aa4d328b
Natalie Raine <nyatalieeee@proton.me>
2024-11-16 15:43:10 -0500

    add color reference

---
+++ assets/colors.sh
@@ -0,0 +1,12 @@
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PINK="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+TEXT="\033[0;39m"
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
